c6c016b8c6351b740d63207af60d3b3f6d2aaac4
seria <seria.ati@gmail.com>
2025-02-23 17:35:24 +0800

    test: Add return types

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -6,14 +6,14 @@ import stock_crawl
 
 
 @pytest.mark.asyncio
-async def test_fetch_stocks():
+async def test_fetch_stocks() -> None:
     async with stock_crawl.StockCrawl() as client:
         stocks = await client.fetch_stocks()
         assert len(stocks) > 0
 
 
 @pytest.mark.asyncio
-async def test_fetch_main_forces():
+async def test_fetch_main_forces() -> None:
     async with stock_crawl.StockCrawl() as client:
         for recent_day in stock_crawl.RecentDay:
             main_forces = await client.fetch_main_forces(
@@ -23,7 +23,7 @@ async def test_fetch_main_forces():
 
 
 @pytest.mark.asyncio
-async def test_fetch_force_buy_sells():
+async def test_fetch_force_buy_sells() -> None:
     async with stock_crawl.StockCrawl() as client:
         force_buy_sells = await client.fetch_force_buy_sells(
             "https://fubon-ebrokerdj.fbs.com.tw/z/zc/zco/zco0/zco0.djhtm?a=2330&b=9600&BHID=9600"
@@ -32,14 +32,14 @@ async def test_fetch_force_buy_sells():
 
 
 @pytest.mark.asyncio
-async def test_fetch_company_capitals():
+async def test_fetch_company_capitals() -> None:
     async with stock_crawl.StockCrawl() as client:
         company_capitals = await client.fetch_company_capitals()
         assert len(company_capitals) > 0
 
 
 @pytest.mark.asyncio
-async def test_fetch_history_trades():
+async def test_fetch_history_trades() -> None:
     async with stock_crawl.StockCrawl() as client:
         history_trades = await client.fetch_history_trades("2330")
         assert len(history_trades) > 0
@@ -49,35 +49,35 @@ async def test_fetch_history_trades():
 
 
 @pytest.mark.asyncio
-async def test_fetch_dividend_days():
+async def test_fetch_dividend_days() -> None:
     async with stock_crawl.StockCrawl() as client:
         dividend_days = await client.fetch_dividend_days()
         assert len(dividend_days) > 0
 
 
 @pytest.mark.asyncio
-async def test_fetch_punish_stocks():
+async def test_fetch_punish_stocks() -> None:
     async with stock_crawl.StockCrawl() as client:
         punish_stocks = await client.fetch_punish_stocks()
         assert len(punish_stocks) > 0
 
 
 @pytest.mark.asyncio
-async def test_fetch_news():
+async def test_fetch_news() -> None:
     async with stock_crawl.StockCrawl() as client:
         news = await client.fetch_news()
         assert len(news) > 0
 
 
 @pytest.mark.asyncio
-async def test_fetch_most_recent_trade_day():
+async def test_fetch_most_recent_trade_day() -> None:
     async with stock_crawl.StockCrawl() as client:
         most_recent_trade_day = await client.fetch_most_recent_trade_day()
         assert isinstance(most_recent_trade_day, datetime.date)
 
 
 @pytest.mark.asyncio
-async def test_fetch_stock():
+async def test_fetch_stock() -> None:
     async with stock_crawl.StockCrawl() as client:
         stock = await client.fetch_stock("2330")
         assert stock is not None
@@ -90,21 +90,21 @@ async def test_fetch_stock():
 
 
 @pytest.mark.asyncio
-async def test_fetch_stock_ids():
+async def test_fetch_stock_ids() -> None:
     async with stock_crawl.StockCrawl() as client:
         stock_ids = await client.fetch_stock_ids()
         assert len(stock_ids) > 0
 
 
 @pytest.mark.asyncio
-async def test_fetch_stock_ids_only_four_digits():
+async def test_fetch_stock_ids_only_four_digits() -> None:
     async with stock_crawl.StockCrawl() as client:
         stock_ids = await client.fetch_stock_ids(only_four_digits=True)
         assert all(len(stock_id) == 4 for stock_id in stock_ids)
 
 
 @pytest.mark.asyncio
-async def test_client_reuse():
+async def test_client_reuse() -> None:
     async with stock_crawl.StockCrawl() as client:
         await client.fetch_history_trades("2330", limit=1)
         await client.fetch_history_trades("2330", limit=1)
